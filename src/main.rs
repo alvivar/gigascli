8e4406da5a956eadc6a01b0bc4d3523ab7b09802
Andrés Villalobos <andresalvivar@gmail.com>
2020-12-06 03:55:18 -0600

    Decent analysis

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::{App, AppSettings::ArgRequiredElseHelp, Arg, SubCommand};
-use std::fs::File;
 use std::io::{BufRead, BufReader, Error, Write};
+use std::{collections::HashMap, fs::File};
 use std::{env, path::Path};
 use walkdir::{DirEntry, WalkDir};
 
@@ -49,7 +49,7 @@ fn main() {
     // Code Generation
 
     if let Some(_matches) = matches.subcommand_matches("generate") {
-        println!("Generating EntitySet.cs[...]")
+        println!("Generating EntitySet.cs[...]");
     }
 
     // Code Analizis
@@ -68,36 +68,90 @@ fn main() {
             }
         }
 
-        // Looking for !gigas and collecting classes
+        // Look and classify !gigas & !alt on files
 
-        let mut gigas_classes: Vec<DirEntry> = Vec::new();
-        let mut gigas_alt_classes: Vec<DirEntry> = Vec::new();
-        for entry in csharp_files {
+        let mut gigas: Vec<DirEntry> = Vec::new();
+        let mut gigas_alt: Vec<DirEntry> = Vec::new();
+        for entry in &csharp_files {
             let input = File::open(entry.path()).unwrap();
             let buffered = BufReader::new(input);
             for line in buffered.lines() {
                 let line = line.unwrap();
                 if line.to_lowercase().contains("!gigas") {
                     if line.to_lowercase().contains("!alt") {
-                        gigas_alt_classes.push(entry.clone());
+                        gigas_alt.push(entry.clone());
                     } else {
-                        gigas_classes.push(entry.clone());
+                        gigas.push(entry.clone());
                     }
                     break;
                 }
             }
         }
 
-        //
+        // Debug printing
 
-        println!("Normal classes:");
-        for entry in gigas_classes {
-            println!("{}", entry.file_name().to_string_lossy());
+        // println!("\nGIGAS");
+        // for entry in &gigas {
+        //     println!("{}", entry.file_name().to_string_lossy());
+        // }
+
+        // println!("\nALT");
+        // for entry in &gigas_alt {
+        //     println!("{}", entry.path().file_stem().unwrap().to_string_lossy());
+        // }
+
+        // Find relationships between classes
+
+        let gigas_classes: Vec<&str> = gigas
+            .iter()
+            .map(|x| x.path().file_stem().unwrap().to_str().unwrap())
+            .collect();
+
+        let mut relation: HashMap<String, Vec<String>> = HashMap::new();
+
+        for file in &csharp_files {
+            for line in lines_from_file(file.path()) {
+                for class in &gigas_classes {
+                    let filename = file.path().file_stem().unwrap().to_str().unwrap();
+
+                    if filename == "Femto" || filename == "EntitySet" {
+                        continue;
+                    }
+
+                    let gigasnames: Vec<String> =
+                        vec![format!("{}s", class), format!("{}Ids", class)];
+
+                    for name in gigasnames {
+                        if line.contains(&name) {
+                            // print!("\nFound {} in {} at \n{}\n", class, filename, line);
+
+                            let classes = relation
+                                .entry(filename.to_string())
+                                .or_insert_with(Vec::new);
+
+                            if !classes.contains(&class.to_string()) {
+                                classes.push(class.to_string());
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        println!("\nAlt classes:");
-        for entry in gigas_alt_classes {
-            println!("{}", entry.file_name().to_string_lossy());
+        for (key, value) in &relation {
+            println!("\n{}", key);
+            for entry in value {
+                println!("\t{}", entry);
+            }
         }
     }
+}
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("No file name.");
+    let buffer = BufReader::new(file);
+    buffer
+        .lines()
+        .map(|l| l.expect("Couldn't parse line."))
+        .collect()
 }
